196e0d1cbae4bab1914c6a9680b36ccc66bb36d6
lodowd21 <60274768+lodowd21@users.noreply.github.com>
2022-02-23 22:41:04 -0500

    x

--- Lan-Way-Roofing/script.js
+++ Lan-Way-Roofing/script.js
@@ -1,23 +1,23 @@
-function start(){
+function start() {
     preloadImage('images/roof.jpg');
 }
 
 function showDiv() {
     document.getElementById('missionStatement').style.display = "block";
- }
+}
 
- function preloadImage(url) {
-     var img=new Image();
-     img.src=url;
- }
- 
+function preloadImage(url) {
+    var img = new Image();
+    img.src = url;
+}
+
 
 function showDiv2() {
     document.getElementById('contact').style.display = "block";
- }
+}
 
 
- function thankyouMessage(){
+function thankyouMessage() {
     let x = document.forms["requestForm"]["name"].value;
     let y = document.forms["requestForm"]["email"].value;
     let z = document.forms["requestForm"]["address"].value;
@@ -25,11 +25,11 @@ function showDiv2() {
     if (x != "" && y != "" && z != "") {
         alert("Thank you, we will be in touch shortly!");
         return false;
-      }
+    }
 
- }
+}
 
- function getWeather(){
+function getWeather() {
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
     xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=london&appid=f4c546f898560a59b0cd737648f02285");
@@ -37,18 +37,18 @@ function showDiv2() {
     xhr.send();
     xhr.onload = () => {
         console.log(xhr)
-        if(xhr.status === 200){
+        if (xhr.status === 200) {
             console.log(JSON.parse(xhr.response));
             alert(`The weather api worked ${xhr.status}  ${xhr.statusText}`);
-                } else {
+        } else {
             console.log(`error ${xhr.status} ${xhr.statusText}`)
             alert(`The weather api failed ${xhr.status}  ${xhr.statusText}`);
 
         }
-     }
- }
+    }
+}
 
- function getLogicApp(){
+function getLogicApp() {
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
     xhr.open("GET", "https://lanway-logicapp1.azurewebsites.net:443/api/lanway-la-validate-user/triggers/manual/invoke?api-version=2020-05-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=MfH_Rxu-2s_5OhuwaUKsZWxwUWRO2tdkzqMV7VPl5HY");
@@ -56,23 +56,25 @@ function showDiv2() {
     xhr.send();
     xhr.onload = () => {
         console.log(xhr)
-        if(xhr.status === 200){
+        if (xhr.status === 200) {
             console.log(JSON.parse(xhr.response));
             alert(`The logic app api worked ${xhr.status}  ${xhr.statusText}`);
-                } else {
+        } else {
             console.log(`error ${xhr.status} ${xhr.statusText}`)
             alert(`The logic app api failed ${xhr.status}  ${xhr.statusText}`);
 
         }
-     }
- }
+    }
+}
+
+
 
 function validateUser() {
     let x = document.forms["login"]["username"].value;
     let y = document.forms["login"]["password"].value;
 
     if (x == "") {
-        alert("Username must be filled out");
+        alert("Username7 must be filled out");
         return false;
     }
 
@@ -81,28 +83,33 @@ function validateUser() {
         return false;
     }
 
+    /* test validation
     if (x != "lodowd" || y != "hotdog"){
         alert("incorrect user or pass");
         return false;
     }
-
+    */
 
     var xhr = new XMLHttpRequest();
+    var loginData = JSON.stringify({"username": x, "pass": y});
     xhr.withCredentials = false;
-    xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=london&appid=f4c546f898560a59b0cd737648f02285");
+    xhr.open("POST", "https://lanway-logicapp1.azurewebsites.net:443/api/lanway-la-admin-login/triggers/manual/invoke?api-version=2020-05-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9VrhMwMgkDGkcwhOQmI5UAkSFrT6pVbtB13P9NE42Yo");
     //xhr.setRequestHeader('Access-Control-Allow-Origin','https://openweathermap.org/api');
-    xhr.send();
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(loginData);
     xhr.onload = () => {
         console.log(xhr)
-        if(xhr.status === 200){
+        var isLoginSuccessful = JSON.parse(xhr.response)
+        console.log(isLoginSuccessful.isValid)
+        if (xhr.status === 200 && isLoginSuccessful.isValid === true) {
             console.log(JSON.parse(xhr.response));
             alert(`Successful login ${xhr.status}  ${xhr.statusText}`);
-            window.location.href="admin_functions.html";
-                } else {
+            window.location.href = "admin_functions.html";
+        } else {
             console.log(`error ${xhr.status} ${xhr.statusText}`)
-            alert(`Username or password incorrect ${xhr.status}  ${xhr.statusText}`);
+            alert(`Username or password incorrect ${xhr.status}  ${xhr.statusText} ld: ${loginData} x: ${x}  y: ${y}`);
 
         }
-     }
+    }
 
 }
